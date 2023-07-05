e28930469ae5b657f0ff217131fed401aa257606
NnSToP <mr.khadir.med1@gmail.com>
2023-07-05 16:05:07 -0700

    Recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+<<<<<<< HEAD
+int _putchar(char c);
+void _puts_recursion(char *s);
+=======
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+>>>>>>> 4d8b61471a8385c7e896e4ed40550d197f19d016
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+<<<<<<< HEAD
+int wildcmp(char *s1, char *s2);
+=======
+>>>>>>> 4d8b61471a8385c7e896e4ed40550d197f19d016
+int prime(int n, int i);
+#endif
